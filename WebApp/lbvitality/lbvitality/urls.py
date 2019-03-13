26f1ef398ea368ab8a3f6068a30447f91ef507e8
Ernest <arreern@gmail.com>
2019-03-13 14:52:29 -0700

    OK1

---
+++ WebApp/lbvitality/lbvitality/urls.py
@@ -0,0 +1,35 @@
+"""lbvitality URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+<<<<<<< HEAD:WebApp/backend/backend/urls.py
+from django.urls import path
+from rest_framework import routers
+from LBVitality import views
+
+router = routers.DefaultRouter()
+router.register(r'LBVitality', views.TodoView, 'LBVitality')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
+=======
+from django.urls import path, include
+
+urlpatterns = [
+    path('',include('frontend.urls')),
+    path('', include('businesses.urls'))
+>>>>>>> parent of 0690ba9d... Changes:WebApp/lbvitality/lbvitality/urls.py
+]
