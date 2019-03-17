2cd4b8a96a9a3f60e74ce1772794da3653c73909
David L <dongxli710@gmail.com>
2019-03-17 01:02:01 -0700

    API call

--- WebApp/lbvitality/businesses/urls.py
+++ WebApp/lbvitality/businesses/urls.py
@@ -1,5 +1,7 @@
 from rest_framework import routers
+from .api import BusinessViewSet
 
 router = routers.DefaultRouter()
+router.register('api/businesses', BusinessViewSet, 'Businesses')
 
 urlpatterns = router.urls
