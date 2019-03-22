190a4e049f76c74338f68f5782d15a75ba88f2be
Sovathana Heng <sovathana@Sovathanas-MacBook-Pro.local>
2019-03-22 04:03:17 -0700

    add PostgreSQL

--- Vitality/backend/lbvitality/business/data.py
+++ Vitality/backend/lbvitality/business/data.py
@@ -1,25 +1,24 @@
 import csv
+import psycopg2
 import os
-import sqlite3
 
-my_list = []
 
-dbPath = '/'.join((os.path.dirname(__file__)+'').split('/')[0:-1]) + '/db.sqlite3'
+conn = psycopg2.connect("host=127.0.0.1 dbname=businessdb user=postgres password=admin")
+cur = conn.cursor()
 
-db = sqlite3.connect(dbPath)
-cursor = db.cursor()
 
-with open(os.path.abspath(os.path.dirname(__file__)) + '/data/business.csv', 'r') as csvFile:
-    next(csvFile)
-    reader = csv.reader(csvFile)
+
+with open('/'.join((os.path.dirname(__file__)+'').split('/')[0:-4]) + '/Data/business.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Skip the header row.
     for row in reader:
-        cursor.execute('''INSERT INTO business_business (licenseNum,name,licenseType,status,processedDate,startDate,
-            expireDate,employeeNum,companyType,businessType,propertyType,zipcode) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)''', 
-            (row[0],row[1],row[3],row[5],row[6],row[7],row[8],row[17],row[16],row[22],row[13],row[15]))
-        
-db.commit()
+        cur.execute(
+            """INSERT INTO business_business ("licenseNum","name","licenseType","status","processedDate","startDate", \
+            "expireDate","employeeNum","companyType","businessType","propertyType","zipcode") \
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (row[0],row[1],row[3],row[5],row[6],row[7],row[8],row[17],row[16],row[22],row[13],row[15])
+        )
 
+        #print('license#:', row[0],' name:',row[1],' licType:',row[3],' status:',row[5],' name:',row[6],row[7],row[8],row[17],row[16],row[22],row[13],row[15])
 
+conn.commit()
 
-print('DONE')
-   
