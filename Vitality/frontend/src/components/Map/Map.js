449e304c6843f79a885764fe09666ffda9cbb652
Ernest <arreern@gmail.com>
2019-04-12 23:26:39 -0700

    Map

---
+++ Vitality/frontend/src/components/Map/Map.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { compose, withProps } from "recompose"
+
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  KmlLayer,
+} = require("react-google-maps");
+const Map = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCys__gg8EEH6Mor2NnnVYL8Y5qukV_mI4",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `86%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+) (props =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat:33.7971,lng:-118.1637 }}
+  >
+    <KmlLayer
+      url={"https://sites.google.com/site/longbeachprojectqwer/kml/City_Of_Long_Beach_City_Boundary.kml"}
+      options={{ preserveViewport: true }}
+    />
+  </GoogleMap>
+);
+function generateRandom() {
+  return Math.random() * 10000000000000000
+}
+export default Map
