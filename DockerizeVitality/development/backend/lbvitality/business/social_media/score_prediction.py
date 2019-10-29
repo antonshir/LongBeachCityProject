021d80517cbf2c91277f49d57bc1f2214b2953e6
andreology <37252145+andreology@users.noreply.github.com>
2019-10-28 21:34:49 -0700

    adding migr, analysi, and desig

---
+++ DockerizeVitality/development/backend/lbvitality/business/social_media/score_prediction.py
@@ -0,0 +1,195 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import csv
+import os
+import django
+import sys
+from datetime import datetime
+
+sys.path.append('/'.join((os.path.dirname(os.path.abspath(__file__)) +
+                          '').split('/')[0:-5]) +
+                '/Vitality/backend/lbvitality')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lbvitality.settings')
+django.setup()
+
+from business.models import *
+
+
+def get_weights():
+    colnames = ['target']
+    colnames1 = ['yelp_review_count', 'yelp_rating', 'google_rating']
+
+    df = pd.read_csv('/'.join(
+        (os.path.dirname(os.path.abspath(__file__)) + '').split('/')[0:-5]) +
+                     '/LogReg/new_tar.csv',
+                     names=colnames)
+    df1 = pd.read_csv('/'.join(
+        (os.path.dirname(os.path.abspath(__file__)) + '').split('/')[0:-5]) +
+                      '/LogReg/new_standardized_data.csv',
+                      names=colnames1)
+
+    y = np.array(df.target.tolist())
+    X = np.array(df1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0,
+                                                        random_state=0)
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+
+    return logreg.coef_  # returns a matrix of weights (coefficients)
+
+
+def get_target(features, weights):
+
+    max = 0
+    target = 0
+
+    for i in range(len(weights)):
+        dot_prod = np.dot(weights[i], features)
+        if dot_prod > max:
+            max = dot_prod
+            target = i
+
+    return target
+
+
+def predict(features, weights):
+    # features is an array that consists of num of days business has been
+    # in business, the number of yelp review, yelp rating, and google rating
+
+    # features = []
+
+    # array of arrays that contains weights for every class
+    # weights = []
+
+    # the features array needs to be in numpy format
+    features = np.array(features)
+    # the weights array needs to be in numpy format
+    weights = np.array(weights)
+
+    #target 3 classes
+    # 0 = bad
+    # 1 = mediocre
+    # 2 = good
+
+    score = get_target(features, weights)
+    print(score)
+    return score
+
+
+if __name__ == "__main__":
+
+    #delete all previous tables(we had dummy scores)
+    SocialMediaScore.objects.all().delete()
+    # get the weights
+    weights = get_weights()
+
+    #get latest date
+    yelp = YelpHistory.objects.latest('date')
+    date = yelp.date
+
+    businesses = Business.objects.all()
+
+    #loop through all businesses
+    for b in businesses:
+
+        #see if it has yelp (socialmedia)
+        try:
+            yelp_history = YelpHistory.objects.get(yelp=b.yelp, date=date)
+            temp_days_in_business = datetime.today().date() - b.start_date
+
+            #see if it has google too
+            try:
+
+                google_history = GoogleHistory.objects.get(date=date,
+                                                           google=b.google)
+
+                features = [
+                    (yelp_history.review_count - 121.4111111) / 252.1274822,
+                    (yelp_history.rating - 4.027777778) / 0.9521150645,
+                    (google_history.rating - 3.915555556) / 1.436358622
+                ]
+                print(features)
+                score = predict(features, weights)
+                social_media_score = SocialMediaScore(score=score,
+                                                      date=date,
+                                                      business=b)
+                print(social_media_score)
+                print(score)
+                social_media_score.save()
+
+            #no google, make the google features 0
+            except:
+                features = [
+                    (yelp_history.review_count - 121.4111111) / 252.1274822,
+                    (yelp_history.rating - 4.027777778) / 0.9521150645, 0
+                ]
+                print(features)
+                score = predict(features, weights)
+                social_media_score = SocialMediaScore(score=score,
+                                                      date=date,
+                                                      business=b)
+                print(social_media_score)
+                print(score)
+                social_media_score.save()
+        #assign all businesses without socialmedia with a 0
+        except:
+            social_media_score = SocialMediaScore(score=0,
+                                                  date=date,
+                                                  business=b)
+            print(social_media_score)
+            social_media_score.save()
+
+    # #find the latest date in database based on yellp
+    # yelp = YelpHistory.objects.latest('date')
+    # date = yelp.date
+
+    # #find all the yelp histories on that date
+    # yelps_history = YelpHistory.objects.filter(date=date)
+
+    # #for each yelp data, find the google data
+    # for i in yelps_history:
+    #     temp_business = i.yelp.business
+    #     temp_days_in_business = datetime.today().date(
+    #     ) - temp_business.start_date
+
+    #     # change days in business after it is fixed
+    #     # google ratings for that business
+    #     try:
+    #         google_history = GoogleHistory.objects.get(
+    #             date=date, google__business=temp_business)
+
+    #         features = [
+    #             (temp_days_in_business.days - 6751.322222) / 3478.709033,
+    #             (i.review_count - 121.4111111) / 252.1274822,
+    #             (i.rating - 4.027777778) / 0.9521150645,
+    #             (google_history.rating - 3.915555556) / 1.436358622
+    #         ]
+    #         print(features)
+    #         score = predict(features, weights)
+    #         social_media_score = SocialMediaScore(score=score,
+    #                                               date=date,
+    #                                               business=temp_business)
+    #         print(social_media_score)
+    #         print(score)
+    #         social_media_score.save()
+
+    #     # no google ratings for that business
+    #     except:
+    #         features = [
+    #             (temp_days_in_business.days - 6751.322222) / 3478.709033,
+    #             (i.review_count - 121.4111111) / 252.1274822,
+    #             (i.rating - 4.027777778) / 0.9521150645, 0
+    #         ]
+    #         print(features)
+    #         score = predict(features, weights)
+    #         social_media_score = SocialMediaScore(score=score,
+    #                                               date=date,
+    #                                               business=temp_business)
+    #         print(social_media_score)
+    #         print(score)
+    #         social_media_score.save()
---
+++ DockerizeVitality/development/backend/lbvitality/business/social_media/yelp.py
@@ -0,0 +1,224 @@
+#Gets matching businesses in active.csv and stores info in yelp.csv
+
+from __future__ import print_function
+
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+from urllib.parse import quote
+
+import psycopg2
+import csv
+import datetime
+
+#put in your own yelp api key
+API_KEY = ''
+
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+MATCH_PATH = '/v3/businesses/matches'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+
+SEARCH_LIMIT = 3
+
+
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    # print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search(api_key, term, location):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'name': term.replace(' ', '+'),
+        'address1': location.replace(' ', '+'),
+        'city': 'Long Beach',
+        'state': 'US',
+        'country': 'US',
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, MATCH_PATH, api_key, url_params=url_params)
+
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+    Args:
+        business_id (str): The ID of the business to query.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key)
+
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    response = search(API_KEY, term, location)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print(u'No businesses for {0} in {1} found.'.format(term, location))
+        return
+
+    business_id = businesses[0]['id']
+
+    # print(u'{0} businesses found, querying business info ' \
+    #     'for the top result "{1}" ...'.format(
+    #         len(businesses), business_id))
+    response = get_business(API_KEY, business_id)
+
+    # print(u'Result for business "{0}" found:'.format(business_id))
+    pprint.pprint(response, indent=2)
+
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=businessdb user=postgres password=admin")
+    cur = conn.cursor()
+
+    postgreSQL_select_Query = "select * from business_business"
+    cur.execute(postgreSQL_select_Query)
+    # print("Selecting rows from mobile table using cursor.fetchall")
+    businesses = cur.fetchall()
+    d = datetime.datetime.now()
+    date = d.date()
+
+    with open('/Users/David/Desktop/yelp.csv', 'w') as appendFile:
+        writer = csv.writer(appendFile)
+        header = [
+            'licenseNum', 'date', 'yelp_name', 'yelp_id', 'image_url',
+            'is_claimed', 'is_closed', 'address', 'city', 'state', 'country',
+            'zip_code', 'price', 'rating', 'review_count', 'transactions',
+            'url'
+        ]
+        writer.writerow(header)
+
+        count = 0
+        total = 0
+        for row in businesses:
+            total = total + 1
+            if total >= 4900:
+                break
+            print(total)
+            #4435
+            business = search(API_KEY, row[2], row[-2])
+            try:
+                if len(business['businesses']) != 0:
+                    count = count + 1
+                    print("count")
+                    print(count)
+                    info = get_business(API_KEY,
+                                        business['businesses'][0]['id'])
+                    # pprint.pprint(info)
+                    arr = []
+                    try:
+                        arr.append(row[0])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(str(date))
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['name'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['id'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['image_url'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['is_claimed'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['is_closed'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['location']['address1'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['location']['city'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['location']['state'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['location']['country'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['location']['zip_code'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['price'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['rating'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['review_count'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['transactions'])
+                    except:
+                        arr.append('')
+                    try:
+                        arr.append(info['url'])
+                    except:
+                        arr.append('')
+
+                    writer.writerow(arr)
+            except:
+                print(business)
+    print(total)
+    appendFile.close()
