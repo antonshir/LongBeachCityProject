7efa3087f0a39f91d985d1c2dfce49acec1e6d11
andreology <37252145+andreology@users.noreply.github.com>
2019-10-31 22:55:39 -0700

    updating admin

--- Vitality/backend/lbvitality/business/admin.py
+++ Vitality/backend/lbvitality/business/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(CompanyRating)
+admin.site.register(Business)
+admin.site.register(SocialMediaScore)
+admin.site.register(Yelp)
+admin.site.register(YelpHistory)
+admin.site.register(Google)
+admin.site.register(GoogleHistory)
