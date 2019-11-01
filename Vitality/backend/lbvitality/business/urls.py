2071821bdcb7f096b1d4cf0b7b867a46db12f678
andreology <37252145+andreology@users.noreply.github.com>
2019-10-31 22:57:01 -0700

    updating urls

---
+++ Vitality/backend/lbvitality/business/urls.py
@@ -0,0 +1,17 @@
+from rest_framework import routers
+from .views import *
+from django.urls import path
+
+router = routers.DefaultRouter()
+router.register('business', BusinessViewSet, 'business')
+router.register('businesslist', BusinessListViewSet, 'businesslist')
+router.register('zipcoderatio', ZipCodeRatioViewSet, 'zipcoderatio')
+router.register('socialmediascore', SocialMediaScoreViewSet,
+                'socialmediascore')
+router.register('yelphistory', YelpHistoryViewSet, 'yelphistory')
+router.register('googlehistory', GoogleHistoryViewSet, 'googlehistory')
+router.register('companys', CompanyViewSet)
+router.register('companyratings', CompanyRatingViewSet)
+
+
+urlpatterns = router.urls
