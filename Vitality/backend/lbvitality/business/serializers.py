2d80dfae30686c7b00f432b850f3c890db3a6200
andreology <37252145+andreology@users.noreply.github.com>
2019-11-02 16:47:18 -0700

    updating serializer for overwrite create and update method for company rating

--- Vitality/backend/lbvitality/business/serializers.py
+++ Vitality/backend/lbvitality/business/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 #extra_kwargs for hiding password and only allows user to send passwd but never send in api
 class UserSerializer(serializers.ModelSerializer):
@@ -9,9 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ( 'id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
-    #overwrite:validate user 
+    #overwrite:validate user
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 class CompanySerializer(serializers.ModelSerializer):
