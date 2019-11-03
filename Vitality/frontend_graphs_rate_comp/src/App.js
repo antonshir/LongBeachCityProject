d9406645a36ebd19ebe2b23854ea83c665d79c33
andreology <37252145+andreology@users.noreply.github.com>
2019-11-02 23:54:18 -0700

    adding avg rating and count to stars

--- Vitality/frontend_graphs_rate_comp/src/App.js
+++ Vitality/frontend_graphs_rate_comp/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CompanyList from './components/company-list';
+import CompanyDetails from './components/company-details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+
+  state = {
+      companys: [],
+      selectedCompany: null
+  }
+
+  componentDidMount() {
+    //fetch data
+    fetch('http://127.0.0.1:8000/api/companys/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token ad40f2637372be950afb452b5ae14fed8fd50e94'
+      }
+    }).then( resp => resp.json())
+    .then( res => this.setState({companys: res}))
+    .catch( error => console.log(error))
+  }
+
+  loadCompany = company => {
+    this.setState({selectedCompany: company});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Company Rater</h1>
+        <div className="layout">
+        <CompanyList companys={this.state.companys} companyClicked={this.loadCompany}/>
+        <CompanyDetails company={this.state.selectedCompany} updateCompany={this.loadCompany}/>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
