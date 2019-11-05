6d873c8f5d3faecb5f8f2d4e79b38b214dea49c5
andreology <37252145+andreology@users.noreply.github.com>
2019-11-05 11:55:29 -0800

    updating fetch method

--- Vitality/frontend_graphs_rate_comp/src/App.js
+++ Vitality/frontend_graphs_rate_comp/src/App.js
@@ -1,22 +1,24 @@
+import CompanyList from './components/company-list';
+import CompanyDetails from './components/company-details';
+import BusinessList from './components/business-list';
+
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CompanyList from './components/company-list';
-import CompanyDetails from './components/company-details';
 
 class  App extends Component {
 
-  state = {
-      companys: [],
-      selectedCompany: null
-  }
+    state = {
+        companys: [],
+        selectedCompany: null
+    }
 
   componentDidMount() {
     //fetch data
     fetch('http://127.0.0.1:8000/api/companys/', {
       method: 'GET',
       headers: {
-        'Authorization': 'Token ad40f2637372be950afb452b5ae14fed8fd50e94'
+        'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
       }
     }).then( resp => resp.json())
     .then( res => this.setState({companys: res}))
@@ -25,15 +27,20 @@ class  App extends Component {
 
   loadCompany = company => {
     this.setState({selectedCompany: company});
+
+
   }
 
   render() {
+      console.log("app");
+      console.log(this.state.companys);
     return (
       <div className="App">
         <h1>Company Rater</h1>
         <div className="layout">
-        <CompanyList companys={this.state.companys} companyClicked={this.loadCompany}/>
-        <CompanyDetails company={this.state.selectedCompany} updateCompany={this.loadCompany}/>
+                <CompanyList companys={this.state.companys} companyClicked={this.loadCompany} />
+                <CompanyDetails company={this.state.selectedCompany} updateCompany={this.loadCompany}  />
+
         </div>
       </div>
     );
