0432189dfb6348309f206e70828cd36fa5bc0c2b
andreology <37252145+andreology@users.noreply.github.com>
2019-11-05 11:52:34 -0800

    adding bus-list comp to details in company comp

--- Vitality/frontend_graphs_rate_comp/src/components/company-details.js
+++ Vitality/frontend_graphs_rate_comp/src/components/company-details.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import BusinessList from './business-list';
+
 var FontAwesome = require('react-fontawesome');
 
 class CompanyDetails extends Component {
 
   state = {
-    highlighted: -1
+    highlighted: -1,
+    businesses: [],
+     data: {}
   }
 
-  highlightRate = high => evt => {
-    this.setState({highlighted: high});
-  }
+   highlightRate = high => evt => {
+     this.setState({highlighted: high});
+   }
 
-  rateClicked = stars => evt => {
+    rateClicked = stars => evt => {
+       
       fetch(`${process.env.REACT_APP_API_URL}/api/companys/${this.props.company.id}/rate_company/`, {
         method: 'POST',
         headers: {
@@ -25,26 +30,45 @@ class CompanyDetails extends Component {
 
   }
 
-  getDetails = () => {
+  getDetails = (props) => {
+    
     fetch(`${process.env.REACT_APP_API_URL}/api/companys/${this.props.company.id}/`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Token ad40f2637372be950afb452b5ae14fed8fd50e94'
+        'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
       }
       }).then( resp => resp.json())
-      .then( res => this.props.updateCompany(res))
-      .catch( error => console.log(error))
-  }
+        .then(res => this.props.updateCompany(res))
+          .catch(error => console.log(error))
+   }
+
+    getList = () => {
+        fetch(`http://127.0.0.1:8000/api/companys/271/get_bus_list/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
+            }
+        }).then(resp => resp.json())
+            .then(res => this.setState({ businesses: res.result }))
+            .catch(error => console.log(error))
+    }
 
   render() {
-    const comp = this.props.company;
+      
+      if (this.props.company)
+          this.getList();
+      const comp = this.props.company;
+      console.log("comp-detail-method");
+      console.log(this.state.businesses);
 
     return (
       <React.Fragment>
         { comp ? (
           <div>
             <h3>{comp.name}</h3>
+
             <FontAwesome name="star" className={comp.avg_rating > 0 ? 'orange': ''}/>
             <FontAwesome name="star" className={comp.avg_rating > 1 ? 'orange': ''}/>
             <FontAwesome name="star" className={comp.avg_rating > 2 ? 'orange': ''}/>
@@ -59,7 +83,11 @@ class CompanyDetails extends Component {
                 return <FontAwesome key={i} name="star" className={this.state.highlighted > i -1 ? 'purple': ''}
                     onMouseEnter={this.highlightRate(i)} onMouseLeave={this.highlightRate(-1)} onClick={this.rateClicked(i)}/>
               })}
-            </div>
+             </div>
+
+             <h3>Businesses Owned By {this.props.company.name} </h3>
+              <BusinessList list={this.state.businesses} comp={this.props.company} />
+            
           </div>
         ) : null }
       </React.Fragment>
