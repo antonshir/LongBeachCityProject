04d8051596f7a832415529aef784c9835578707d
andreology <37252145+andreology@users.noreply.github.com>
2019-11-07 06:29:11 -0800

    adding chart-wrapper component to begin testing

--- Vitality/frontend_graphs_rate_comp/src/App.js
+++ Vitality/frontend_graphs_rate_comp/src/App.js
@@ -1,6 +1,7 @@
 import CompanyList from './components/company-list';
 import CompanyDetails from './components/company-details';
 import BusinessList from './components/business-list';
+import ChartWrapper from './components/chart-wrapper';
 
 import React, {Component} from 'react';
 import logo from './logo.svg';
@@ -10,38 +11,56 @@ class  App extends Component {
 
     state = {
         companys: [],
-        selectedCompany: null
+        selectedCompany: null,
+        graphsData: []
     }
 
-  componentDidMount() {
-    //fetch data
-    fetch('http://127.0.0.1:8000/api/companys/', {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
-      }
-    }).then( resp => resp.json())
-    .then( res => this.setState({companys: res}))
-    .catch( error => console.log(error))
-  }
+    componentDidMount() {
+        //fetch data
+        fetch('http://127.0.0.1:8000/api/companys/', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
+            }
+        }).then(resp => resp.json())
+            .then(res => this.setState({ companys: res }))
+            .catch(error => console.log(error))
+
+
+        fetch(`http://127.0.0.1:8000/api/companys/2360/get_bus_list/`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token ba809f29097d05d76e97e0b9ea3ed7d007f18105'
+            }
+        }).then(resp => resp.json())
+            .then(res => this.setState({ graphsData: res.result }))
+            .catch(error => console.log(error))
+        
+
+    }
 
   loadCompany = company => {
     this.setState({selectedCompany: company});
 
 
   }
-
-  render() {
-      console.log("app");
-      console.log(this.state.companys);
-    return (
-      <div className="App">
-        <h1>Company Rater</h1>
-        <div className="layout">
+    /*<div className="layout">
                 <CompanyList companys={this.state.companys} companyClicked={this.loadCompany} />
                 <CompanyDetails company={this.state.selectedCompany} updateCompany={this.loadCompany}  />
 
-        </div>
+        </div> */
+
+  render() {
+      console.log("app");
+      console.log(this.state.graphsData);
+    return (
+        <div className="App">
+            {(this.state.graphsData[0]) ? 
+                <ChartWrapper data={this.state.graphsData}/>
+                :  <h1>Company Rater</h1>
+            } 
+        
       </div>
     );
   }
