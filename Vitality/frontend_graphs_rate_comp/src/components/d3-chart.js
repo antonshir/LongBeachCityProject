cf0929a9da329e2f1ee0b5263027581b271a5205
andreology <37252145+andreology@users.noreply.github.com>
2019-11-07 07:21:23 -0800

    created bar graph out of business data using d3

--- Vitality/frontend_graphs_rate_comp/src/components/d3-chart.js
+++ Vitality/frontend_graphs_rate_comp/src/components/d3-chart.js
@@ -1,26 +1,40 @@
 import * as d3 from 'd3';
+const MARGIN = { TOP: 100, BOTTOM: 10, LEFT: 100, RIGHT: 10}
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
 export default class D3Chart {
     constructor(element, data) {
         const svg = d3.select(element)
             .append("svg")
-            .attr("width", 800)
-            .attr("height", 500)
-        console.log("d3chartclassx")
-        console.log(data)
+                .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+            .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+            .append("g")
+            .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
         const y = d3.scaleLinear()
-            .domain([0, 298])
-            .range([0, 500])
+            .domain([0, d3.max(data, d => d.employee_num)])
+            .range([0, HEIGHT])
 
-        console.log(y(298))
+        const x = d3.scaleBand()
+            .domain(data.map(d => d.license_num))
+            .range([0, WIDTH])
+            .padding(0.5)
+
+        const xAxisCall = d3.axisBottom(x)
+        svg.append("g")
+            .attr("transform", `translate(0, ${HEIGHT}`)
+            .call(xAxisCall)
+
+        const yAxisCall = d3.axisLeft(y)
+        svg.append("g").call(yAxisCall)
 
         const rectangles = svg.selectAll("rect").data(data)
         rectangles.enter()
             .append("rect")
-            .attr("x", (d, i) => i * 100 )
-            .attr("y", 0)
-            .attr("width", 100)
+            .attr("x", d => x(d.license_num) )
+            .attr("y", d => HEIGHT - y(d.employee_num))
+            .attr("width", x.bandwidth)
             .attr("height", d => y(d.employee_num) )
             .attr("fill", d => {
                 if (d.zipcode === 90805) {
